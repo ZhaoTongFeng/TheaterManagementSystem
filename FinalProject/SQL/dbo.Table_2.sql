20186d2d5c8da9bafe5c3fa4c94e37858df0e40d
1300889458 <1300889458@qq.com>
2020-09-01 10:42:39 +0800

    Update README

--- FinalProject/SQL/dbo.Table_2.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Order]
-(
-	[Id] INT NOT NULL PRIMARY KEY,
-	[ScheduleID] INT NOT NULL,
-	[SiteID] INT NOT NULL,
-	[PaymentInfo] NVARCHAR(1000) NOT NULL,
-	[State] INT NOT NULL, 
-    CONSTRAINT [FK_Order_ToSchedule] FOREIGN KEY ([ScheduleID]) REFERENCES [Schedule]([Id])
-)
